fdcf0a70b483b96e032f8e9efd00ebe26c10ab7b
Jose Leonardo Giraldo <Jose.giraldogo@gmail.com>
2017-03-04 03:01:48 -0500

    Se creo una vista importante

---
+++ app/Http/Controllers/contralador1.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class contralador1 extends Controller
+{
+    function ejemplo(){
+        return view('ejemplo');
+    }
+
+    function ejemplo(){
+        return view('ejemplo2');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('veame', 'contralador1@ejemplo');
+
